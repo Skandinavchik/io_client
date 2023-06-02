b8e1c8e2cc9397353dbc94cd29a4311d2d370842
Artem Sokolov <sknd2569@gmail.com>
2023-06-02 13:33:29 +0200

    filterUsers func moved to usersSlice

--- src/chat/components/users.js
+++ src/chat/components/users.js
@@ -1,49 +1,52 @@
 import { Box, Avatar } from "@mui/material";
 import { useEffect } from "react";
-import { fetchUsers } from '../slices/usersSlice';
+import { fetchUsers, findUsers } from '../slices/usersSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 
 
 const Users = () => {
 
-    const { users, usersLoadingStatus, queryString } = useSelector(state => state.users);
+    const { users, filteredUsers, usersLoadingStatus, queryString } = useSelector(state => state.users);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(fetchUsers('http://localhost:8000/api/v1.0/users'));
+        dispatch(fetchUsers());
     }, [dispatch]);
 
-    if (usersLoadingStatus === 'error') {
-        return 'Error';
-    }
+    useEffect(() => {
+        dispatch(findUsers(queryString));
+    }, [dispatch, queryString]);
 
-    const regex = new RegExp(queryString, 'i');
+    const renderUsers = (arr) => {
+        return arr.map(item => {
+            return (
+                <Box key={item.userName} sx={{
+                    pl: '20px',
+                    pt: '10px',
+                    pb: '10px',
+                    display: 'flex',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    gap: '10px',
+                    cursor: 'pointer',
+                    '&:hover': {
+                        background: 'rgba(25, 118, 210, 0.04)',
+                    }
+                }}>
+                    <Avatar alt={item.userName} src="#" />
+                    <div>{item.userName}</div>
+                </Box>
+            );
+        });
+    };
 
-    const renderUsers = users.filter(item => regex.test(item.userName)).map(item => {
-        return (
-            <Box key={item.userName} sx={{
-                pl: '20px',
-                pt: '10px',
-                pb: '10px',
-                display: 'flex',
-                justifyContent: 'flex-start',
-                alignItems: 'center',
-                gap: '10px',
-                cursor: 'pointer',
-                '&:hover': {
-                    background: 'rgba(25, 118, 210, 0.04)',
-                }
-            }}>
-                <Avatar alt={item.userName} src="#" />
-                <div>{item.userName}</div>
-            </Box>
-        );
-    });
+    const usersState = queryString === '' || queryString === undefined ? users : filteredUsers;
+    const usersList = renderUsers(usersState);
 
     return (
         <Box sx={{}}>
-            {renderUsers.length ? renderUsers : <div style={{textAlign: 'center'}}>No Users</div>}
+            {usersList.length ? usersList : <div style={{ textAlign: 'center' }}>No Users</div>}
         </Box>
     );
 };
--- src/chat/slices/usersSlice.js
+++ src/chat/slices/usersSlice.js
@@ -5,11 +5,12 @@ import ky from "ky";
 const initialState = {
     usersLoadingStatus: 'idle',
     users: [],
+    filteredUsers: [],
     queryString: '',
 };
 
-const fetchUsers = createAsyncThunk('users/fetchUsers', (url) => {
-    return ky.get(url, {
+const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    return await ky.get('http://localhost:8000/api/v1.0/users', {
         credentials: 'include',
     }).json();
 });
@@ -18,15 +19,20 @@ const usersSlicer = createSlice({
     name: 'users',
     initialState,
     reducers: {
-        handleQueryString: (state, action) => { state.queryString = action.payload }
+        handleQueryString: (state, action) => { state.queryString = action.payload },
+        findUsers: (state, action) => {
+            const regex = new RegExp(action.payload, 'i');
+            state.filteredUsers = state.users.filter(item => regex.test(item.userName));
+        },
     },
 
     extraReducers: builder => {
         builder
             .addCase(fetchUsers.pending, state => { state.usersLoadingStatus = 'loading' })
             .addCase(fetchUsers.fulfilled, (state, action) => {
-                state.users = action.payload.data.users;
                 state.usersLoadingStatus = 'idle';
+                state.users = action.payload.data.users;
+                state.filteredUsers = action.payload.data.users;
             })
             .addCase(fetchUsers.rejected, state => { state.usersLoadingStatus = 'error' })
             .addDefaultCase(() => { });
@@ -35,6 +41,6 @@ const usersSlicer = createSlice({
 
 const { reducer, actions } = usersSlicer;
 
-export const { handleQueryString } = actions;
+export const { handleQueryString, findUsers } = actions;
 export { fetchUsers };
 export default reducer;
